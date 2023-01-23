8998b97e6353aa1c6fcb38860b34a7250c134217
Raeludwig <racheleludwig@gmail.com>
2023-01-23 12:15:30 -0700

    working on fixing the local storage issues

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//TODO-fix the local storage issue -rs
+const saveBtn = document.getElementsByClassName('btn saveBtn col-2 col-md-1');
+var eventInfo = [];
+
+saveBtn.addEventListener('click', (e){
+localStorage.setItem("Events", JSON.stringify(eventInfo));
+});
+
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -11,7 +21,9 @@ $(function () {
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
-
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
 
 
 
@@ -25,9 +37,7 @@ $('#currentDay').text(today.format('dddd, MMMM D'));
 
 
 
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
